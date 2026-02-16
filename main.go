d641b863d9182e34f5d97a4cc6d734ed171bd1fd
BMWmts <aempongsapak@gmail.com>
2026-02-16 22:46:37 +0700

    If_else_stm_example

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ func main() {
 	age := 24
 	fmt.Printf("Name: %s\nAge: %d\n", name, age)
 	theDefaultGopher()
+	ifelse_stm()
 }
 
 func theDefaultGopher() {
@@ -20,7 +21,26 @@ func theDefaultGopher() {
 
 	totalScore := float64(score) + bonus
 
-	fmt.Println("Score: %d \nPassed: %t \nRemark: %q \n", score, isPassed, remark)
-	fmt.Println("Total: ", totalScore)
+	fmt.Printf("Score: %d \nPassed: %t \nRemark: %q \n", score, isPassed, remark)
+	fmt.Printf("Total: %.2f\n", totalScore)
 }
 
+func ifelse_stm() {
+
+	var msg string
+	age := 18
+
+	if age < 18 {
+		msg = "Entry Denied: Too young"
+	} else if age >= 18 && age < 20 {
+		msg = "Entry Allowed: Limited Access (No Alcohol)"
+	} else {
+		msg = "Entry Allowed: Full Access"
+	}
+
+	if age%2 == 0 {
+		msg = msg + " (Lucky Day)"
+	}
+
+	fmt.Println(msg)
+}
